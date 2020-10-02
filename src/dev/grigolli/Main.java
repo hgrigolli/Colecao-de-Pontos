691fb1a1b0ce213b350221859da3cf1962375e66
hgrigolli <henrigrigo@gmail.com>
2020-10-01 22:45:17 -0300

    Exclusao da classe Menu.java Implementado item 1. adicionar no final da coleção

--- src/dev/grigolli/Main.java
+++ src/dev/grigolli/Main.java
@@ -1,7 +1,7 @@
 package dev.grigolli;
 
 import dev.grigolli.colecao.ListaPontos;
-import dev.grigolli.menu.Menu;
+import dev.grigolli.colecao.Ponto;
 
 import javax.swing.*;
 
@@ -22,8 +22,8 @@ public class Main {
                         "4. Remover um elemento em uma posição na coleção\n" +
                         "5. Calcular a distância dos dois pontos mais distantes na coleção\n" +
                         "6. Retornar uma coleção de pontos contido em um círculo\n" +
-                        "7. Sair do programa\n\n");
-
+                        "7. Sair do programa\n\n" +
+                        "Coleção atual: " + listaPontos.toString());
 
                 if(opcaoMenuStr == null || opcaoMenuStr.isBlank()){
                     opcaoMenu = null;
@@ -33,37 +33,62 @@ public class Main {
                 }
             } while(opcaoMenu == null);
 
-            Integer elemento;
+            Ponto elemento = new Ponto(0,0);
             switch(opcaoMenu) {
                 case 1:
 
                     do {
-                        String elementoStr = JOptionPane.showInputDialog(null, "Insira o valor do elemento");
-                        if(elementoStr == null || elementoStr.isBlank()){
-                            elemento = null;
-                            JOptionPane.showMessageDialog(null,"Favor inserir um valor para o elemento");
+                        JTextField x = new JTextField();
+                        JTextField y = new JTextField();
+                        Object[] ponto = {
+                                "X: ", x,
+                                "Y: ", y
+                        };
+
+                        int option = JOptionPane.showConfirmDialog(null, ponto, "Defina o ponto (x,y)", JOptionPane.OK_CANCEL_OPTION);
+
+                        if (option == JOptionPane.OK_OPTION) {
+                            if(x.getText() != null && y.getText() !=null){
+                                elemento = new Ponto(Integer.parseInt(x.getText()), Integer.parseInt(y.getText()));
+                            }
                         } else {
-                            elemento = Integer.parseInt(elementoStr);
+                            elemento = null;
+                            break;
                         }
                     } while(elemento == null);
 
-                    Menu.adicionarElementoFinal(listaPontos, elemento);
+                    listaPontos.adicionaFinal(elemento);
 
                     break;
 
                 case 2:
 
                     do {
-                        String elementoStr = JOptionPane.showInputDialog(null, "Insira o valor do elemento");
-                        if(elementoStr == null || elementoStr.isBlank()){
-                            elemento = null;
-                            JOptionPane.showMessageDialog(null,"Favor inserir um valor para o elemento");
+                        JTextField x = new JTextField();
+                        JTextField y = new JTextField();
+                        JTextField posicao = new JTextField();
+
+                        Object[] ponto = {
+                                "Defina o ponto (x,y) e a posição a ser inserida", null,
+                                "X: ", x,
+                                "Y: ", y,
+                                "Posicao: ", posicao
+                        };
+
+                        int option = JOptionPane.showConfirmDialog(null, ponto, "Defina o ponto (x,y) e a posição a ser inserida", JOptionPane.OK_CANCEL_OPTION);
+
+                        if (option == JOptionPane.OK_OPTION) {
+                            if(x.getText() != null && y.getText() !=null){
+                                elemento = new Ponto(Integer.parseInt(x.getText()), Integer.parseInt(y.getText()));
+
+                            }
                         } else {
-                            elemento = Integer.parseInt(elementoStr);
+                            elemento = null;
+                            break;
                         }
                     } while(elemento == null);
 
-                    Menu.adicionarElementoInicio(listaPontos, elemento);
+                    listaPontos.adicionaFinal(elemento);
 
                     break;
 
@@ -75,11 +100,11 @@ public class Main {
                             elemento = null;
                             JOptionPane.showMessageDialog(null,"Favor inserir um elemento");
                         } else {
-                            elemento = Integer.parseInt(elementoStr);
+                            //elemento = Integer.parseInt(elementoStr);
                         }
                     } while(elemento == null);
 
-                    Menu.buscarIndice(listaPontos, elemento);
+                    //Menu.buscarIndice(listaPontos, elemento);
 
                     break;
 
@@ -90,11 +115,11 @@ public class Main {
                             elemento = null;
                             JOptionPane.showMessageDialog(null,"Favor inserir um valor para o elemento");
                         } else {
-                            elemento = Integer.parseInt(elementoStr);
+                            //elemento = Integer.parseInt(elementoStr);
                         }
                     } while(elemento == null);
 
-                    Menu.removerElemento(listaPontos, elemento);
+                    //Menu.removerElemento(listaPontos, elemento);
 
                     break;
 
--- src/dev/grigolli/colecao/ListaPontos.java
+++ src/dev/grigolli/colecao/ListaPontos.java
@@ -9,6 +9,46 @@ public class ListaPontos {
         this.validos = 0;
     }
 
+    public void adicionaFinal(Ponto ponto){
+        int i = 0;
+        while(this.pontos[i] != null){
+            i++;
+        }
+        if(i > this.validos){
+            return;
+        } else {
+            this.pontos[i] = ponto;
+            this.validos++;
+        }
+    }
+    public void adicionaPosicao(Ponto ponto, int posicao){
+        if(posicao >= this.pontos.length){
+            return;
+        } else if (this.pontos[posicao] != null) {
+            if(this.validos +1 >= this.pontos.length){
+                //Ultrapassa o limite de pontos a ser incluidp
+                return;
+            }
+        }
+
+        if (this.pontos[0] == null){
+            this.pontos[0] = ponto;
+        }
+        //TODO: deslocar para a direita
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder sb = new StringBuilder("[ ");
+        for(Ponto p : this.pontos){
+            if(p != null){
+                sb.append(" (").append(p.getX()).append(",").append(p.getY()).append(") ");
+            }
+        }
+        sb.append(" ]");
+
+        return sb.toString();
+    }
 
 
 }
